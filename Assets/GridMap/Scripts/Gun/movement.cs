0f80e5b5c6dafac6b34ffe5b0baa61b974c942f9
Elijah Yap <yap@usc.edu>
2022-09-25 08:27:50 -0700

    style

--- Assets/GridMap/Scripts/Gun/movement.cs
+++ Assets/GridMap/Scripts/Gun/movement.cs
@@ -8,21 +8,14 @@ public class Movement : MonoBehaviour
     public Rigidbody2D rb;
     Vector2 mousePos;
     public Camera cam;
-    // Start is called before the first frame update
-    // Update is called once per frame
-    void Update()
-    {
-        mousePos=cam.ScreenToWorldPoint(Input.mousePosition);
-
-
+    
+    void Update() {
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
     }
-    void FixedUpdate()
-    {
+    
+    void FixedUpdate() {
         Vector2 lookdir = mousePos-rb.position;
-        float angle =Mathf.Atan2(lookdir.y,lookdir.x)*Mathf.Rad2Deg-90f;
-        rb.rotation=angle;
-
-
-
+        float angle = Mathf.Atan2(lookdir.y,lookdir.x)*Mathf.Rad2Deg-90f;
+        rb.rotation = angle;
     }
 }

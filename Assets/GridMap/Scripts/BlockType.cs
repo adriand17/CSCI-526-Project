e25835fdfd5336e2bf339867429e416f14358d17
Elijah Yap <yap@usc.edu>
2022-09-28 13:33:33 -0700

    split file

---
+++ Assets/GridMap/Scripts/BlockType.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BlockType
+{
+    Water,   // Water block.
+    Bedrock, // Uneditable level structure block.
+    Dirt,    // Player placable block.
+    Mirror,  // Mirror block.
+    Glass,   // Glass block.
+}
--- Assets/GridMap/Scripts/Particle.cs
+++ Assets/GridMap/Scripts/Particle.cs
@@ -2,15 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum BlockType
-{
-    Water,   // Water block.
-    Bedrock, // Uneditable level structure block.
-    Dirt,    // Player placable block.
-    Mirror,  // Mirror block.
-    Glass,   // Glass block.
-}
-
 public enum WaterFlowDirection { 
     Still, 
     Left,  

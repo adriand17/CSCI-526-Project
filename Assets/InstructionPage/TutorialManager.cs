a01a15d4f311ecb23778f54c09d71a340692e0f4
sipatil <94469232+sipatil@users.noreply.github.com>
2022-10-06 23:27:06 -0700

    Update UI popups and added wait time to script

--- Assets/InstructionPage/TutorialManager.cs
+++ Assets/InstructionPage/TutorialManager.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,44 +7,63 @@ public class TutorialManager : MonoBehaviour
 
     static private int popUpIndex = 0;
 
-    void Update(){
+    public float waitTime = 2f;
+
+    void Update()
+    {
         if (popUpIndex >= popUps.Length)
         {
             return;
         }
-        
-        if(popUpIndex == 0) {//Tutorial has just started
-            popUps[popUpIndex].SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Space)){
-                popUps[popUpIndex].SetActive(false);
-                popUpIndex++;
-            }
-        } else if(popUpIndex == 1) {
-            popUps[popUpIndex].SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Mouse0)){
-                popUps[popUpIndex].SetActive(false);
-                popUpIndex++;
-            }
-        } else if (popUpIndex == 2)
+
+        if (waitTime > 0)
+        {
+            waitTime -= Time.deltaTime;
+            return;
+        }
+
+        if (popUpIndex == 0)
         {//Tutorial has just started
             popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
                 popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                waitTime = 2f;
+            }
+        }
+        else if (popUpIndex == 1)
+        {
+            popUps[popUpIndex].SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUps[popUpIndex].SetActive(false);
+                popUpIndex++;
+                waitTime = 4f;
+            }
+        }
+        else if (popUpIndex == 2)
+        {
+            popUps[popUpIndex].SetActive(true);
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                popUps[popUpIndex].SetActive(false);
+                popUpIndex++;
+                waitTime = 4f;
             }
         }
         else if (popUpIndex == 3)
-        {//Tutorial has just started
+        {
             popUps[popUpIndex].SetActive(true);
-            if (Input.GetKeyDown(KeyCode.Mouse0))
+            if (Input.GetKeyDown(KeyCode.Space))
             {
                 popUps[popUpIndex].SetActive(false);
                 popUpIndex++;
+                waitTime = 4f;
             }
         }
         else if (popUpIndex == 4)
-        {//Tutorial has just started
+        {
             popUps[popUpIndex].SetActive(true);
             if (Input.GetKeyDown(KeyCode.Mouse0))
             {
@@ -57,6 +74,6 @@ public class TutorialManager : MonoBehaviour
         }
 
 
-        
+
     }
 }

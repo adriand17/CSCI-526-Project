35c9c2ae3d1b3193f6781e1ed5e430397bde1cca
Neeharika <neeharik@usc.edu>
2022-11-03 20:20:41 -0700

    Alternate Title Screen with AIM of the game
    Made with the intention to
    Link to tutorial and further to level selection screen

---
+++ Assets/InstructionPage/InstrScreen.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InstrScreen : MonoBehaviour
+{    
+    public void LoadInstrcuction()
+    {
+        SceneManager.LoadScene("Title Screen");
+        Debug.Log("Loading Game");
+    }
+}
